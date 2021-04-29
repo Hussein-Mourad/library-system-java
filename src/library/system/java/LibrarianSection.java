a3b4dc160ed25ddc250e89ca0bff99fead82dd47
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-04-30 00:33:44 +0200

    Added Lib

--- src/library/system/java/LibrarianSection.java
+++ src/library/system/java/LibrarianSection.java
@@ -181,10 +181,8 @@ public class LibrarianSection extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LibrarianSection().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new LibrarianSection().setVisible(true);
         });
     }
 
